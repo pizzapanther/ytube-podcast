7bdfb959c3787ace7c0084e4175105db8b09e034
Paul Bailey <paul@neutron.studio>
2026-06-08 03:39:27 -0500

    fix rumble parse

--- src/ytube_podcast/rumble.py
+++ src/ytube_podcast/rumble.py
@@ -1,3 +1,4 @@
+import json
 import httpx
 from bs4 import BeautifulSoup as bs
 
@@ -5,19 +6,18 @@ from feedparser import FeedParserDict
 
 TIMEOUT = 120
 HEADERS = {
-  "User-Agent": "Mozilla/5.0 (iPhone; CPU OS 26_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/26.2 Mobile/14E304 Safari/605.1.15"
+  "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/148.0.0.0 Safari/537.36"
 }
 
 def generate_feed(channel):
   response = httpx.get(f'https://rumble.com/c/{channel}', headers=HEADERS, timeout=TIMEOUT)
   page_soup = bs(response.content, 'html.parser')
-  articles = page_soup.find_all("div", class_="videostream")
+  articles = page_soup.find_all("rum-videos-grid")
+  articles = articles[0].find_all('script')
+  data = json.loads(articles[0].text)
   entries = []
-  for article in articles:
-    if article.find('time') is None:
-      continue
-
-    link = f'https://rumble.com{article.find('a')['href']}'
+  for item in data['items']:
+    link = item['url']
     response = httpx.get(link, headers=HEADERS, timeout=TIMEOUT)
     more_soup = bs(response.content, 'html.parser')
     desc = more_soup.find("p", class_="media-description")
@@ -28,9 +28,9 @@ def generate_feed(channel):
       desc = ''
 
     entries.append(FeedParserDict({
-      'title': article.find('h3').text.strip(),
-      'published': article.find('time')['datetime'],
-      'thumb_url': article.find('img')['src'],
+      'title': item['title'],
+      'published': item['upload_date'],
+      'thumb_url': item['thumb'],
       'link': link,
       'description': desc,
       'summary': desc,
